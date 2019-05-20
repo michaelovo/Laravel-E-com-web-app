25abf585a225ff86d8aaf01fdffe59e76fcd103d
michaelovo <emikeovo@gmail.com>
2019-05-19 19:15:09 -0700

    Set-up admin panel structure(header, footer, siderbar, etc

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -11,12 +11,17 @@ class AdminController extends Controller
       if($request->ismethod('post')){
         $data=$request->input();
         if(Auth::attempt(['email'=>$data['email'],'password'=>$data['password'],'admin'=>'1'])){
-          echo "succes";die;
+        //  echo "succes";die;
+        return redirect('/admin/dashboard');
         }
         else{
           echo "failed";die;
         }
       }
       return view('admin.admin_login');
+    }
+
+    public function dashboard(){
+      return view('admin.dashboard');
     }
 }
