e194be5795757f7545f7542201218552c8c5720e
michaelovo <emikeovo@gmail.com>
2019-05-19 18:24:55 -0700

    Configured admin_login view to use default laravel login process

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,13 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Auth;
 
 class AdminController extends Controller
 {
     //
-    public function login(){
+    public function login(Request $request){
+      if($request->ismethod('post')){
+        $data=$request->input();
+        if(Auth::attempt(['email'=>$data['email'],'password'=>$data['password'],'admin'=>'1'])){
+          echo "succes";die;
+        }
+        else{
+          echo "failed";die;
+        }
+      }
       return view('admin.admin_login');
     }
 }
