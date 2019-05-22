0c2f9e4c88ae4895576a9a4f447c7a677e843af9
michaelovo <emikeovo@gmail.com>
2019-05-22 04:06:05 -0700

    Updated Admin_sidebar for easy navigation

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,7 +7,7 @@ use App\Category;
 class CategoryController extends Controller
 {
     //
-
+	// FUNCTION TO INSERT CATEGORY TO categories TABLE IN DB
     public function addCategory(Request $request){
     	if($request->ismethod('post')){
     		$data=$request->all();
@@ -18,8 +18,7 @@ class CategoryController extends Controller
         	$category->description = $data['description'];
         	$category->url = $data['url'];
         	$category->save();
-        	 return redirect('/admin/add-category')->with('flash_categoryadd_success_msg','New category Added successfully!');
-        	//return redirect('/admin/add-category')->with('flash_categoryadd_success_msg','New category Added successfully!');
+        	 return redirect('/admin/add-category')->with('flash_categoryadd_success_msg','New category Added successfully!');       	
     	}
     	
     	return view('admin.categories.add_category');
--- resources/views/layouts/admin_layout/admin_sidebar.blade.php
+++ resources/views/layouts/admin_layout/admin_sidebar.blade.php
@@ -2,27 +2,22 @@
 <div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
   <ul>
     <li class="active"><a href="index.html"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
-    <li> <a href="charts.html"><i class="icon icon-signal"></i> <span>Charts &amp; graphs</span></a> </li>
-    <li> <a href="widgets.html"><i class="icon icon-inbox"></i> <span>Widgets</span></a> </li>
-    <li><a href="tables.html"><i class="icon icon-th"></i> <span>Tables</span></a></li>
-    <li><a href="grid.html"><i class="icon icon-fullscreen"></i> <span>Full width</span></a></li>
+   
     <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i>
-      <span>Forms</span> <span class="label label-important">3</span></a>
+      <span>Categories</span> <span class="label label-important">2</span></a>
       <ul>
-        <li><a href="form-common.html">Basic Form</a></li>
-        <li><a href="form-validation.html">Form with Validation</a></li>
-        <li><a href="form-wizard.html">Form with Wizard</a></li>
+        <li><a href="{{url('/admin/add-category')}}"><i class="icon icon-plus-sign"></i>Add Category</a></li>
+        <li><a href="{{url('/admin/view-category')}}"><i class="icon icon-eye-open"></i>View categories</a></li>
+        
       </ul>
     </li>
     <li><a href="buttons.html"><i class="icon icon-tint"></i> <span>Buttons &amp; icons</span></a></li>
     <li><a href="interface.html"><i class="icon icon-pencil"></i> <span>Eelements</span></a></li>
-    <li class="submenu"> <a href="#"><i class="icon icon-file"></i> <span>Addons</span> <span class="label label-important">5</span></a>
+    <li class="submenu"> <a href="#"><i class="icon icon-cog"></i> <span>Settings</span> <span class="label label-important">1</span></a>
       <ul>
-        <li><a href="index2.html">Dashboard2</a></li>
-        <li><a href="gallery.html">Gallery</a></li>
-        <li><a href="calendar.html">Calendar</a></li>
-        <li><a href="invoice.html">Invoice</a></li>
-        <li><a href="chat.html">Chat option</a></li>
+        <li><a href="{{url('/admin/settings')}}"><i class="icon icon-edit"></i>Update password</a></li>
+        
+       
       </ul>
     </li>
     <li class="submenu"> <a href="#"><i class="icon icon-info-sign"></i> <span>Error</span>
@@ -34,20 +29,7 @@
         <li><a href="error500.html">Error 500</a></li>
       </ul>
     </li>
-    <li class="content"> <span>Monthly Bandwidth Transfer</span>
-      <div class="progress progress-mini progress-danger active progress-striped">
-        <div style="width: 77%;" class="bar"></div>
-      </div>
-      <span class="percent">77%</span>
-      <div class="stat">21419.94 / 14000 MB</div>
-    </li>
-    <li class="content"> <span>Disk Space Usage</span>
-      <div class="progress progress-mini active progress-striped">
-        <div style="width: 87%;" class="bar"></div>
-      </div>
-      <span class="percent">87%</span>
-      <div class="stat">604.44 / 4000 MB</div>
-    </li>
+    <li><a href="{{url('/logout')}}"><i class="icon icon-off"></i> <span>Logout</span></a></li>
   </ul>
 </div>
 <!--sidebar-menu-->
