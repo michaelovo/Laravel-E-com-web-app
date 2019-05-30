07ae2cebab6f6659a525c351562905a2592741d9
michaelovo <emikeovo@gmail.com>
2019-05-30 05:57:14 -0700

    Added multiple fields\Remove jquery function to matrix and add_attributes blade files

--- public/js/backend_js/matrix.form_validation.js
+++ public/js/backend_js/matrix.form_validation.js
@@ -267,12 +267,30 @@ $(document).ready(function(){
 	});
 	// End ---- sweetAlert for Delete Product
 
-	/*$("#delProduct").click(function(){
-		if(confirm('Are you sure you want to delete this Product ?')){
-			return true; 
-		} 
-		return false;
-	}); */
-
+	// Start ---- To add multiple fields\remove jquery function
+	$(document).ready(function(){
+	    var maxField = 10; //Input fields increment limitation
+	    var addButton = $('.add_button'); //Add button selector
+	    var wrapper = $('.field_wrapper'); //Input field wrapper
+	    var fieldHTML = '<div> <input type="text" name="sku[]" id="sku" placeholder="sku" style="width:120px; margin-right:3px; margin-top:3px;" /><input type="text" name="size[]" id="size" placeholder="size"  style="width:120px; margin-right:3px; margin-top:3px;" /><input type="text" name="price[]" id="price" placeholder="price"  style="width:120px; margin-right:3px; margin-top:3px;" /><input type="text" name="stock[]" id="stock" placeholder="stock"  style="width:120px; margin-right:3px; margin-top:3px;" /><a href="javascript:void(0);" class="remove_button icon icon-minus-sign "> Remove</a></div>'; //New input field html 
+	    var x = 1; //Initial field counter is 1
+	    
+	    //Once add button is clicked
+	    $(addButton).click(function(){
+	        //Check maximum number of input fields
+	        if(x < maxField){ 
+	            x++; //Increment field counter
+	            $(wrapper).append(fieldHTML); //Add field html
+	        }
+	    });
+	    
+	    //Once remove button is clicked
+	    $(wrapper).on('click', '.remove_button', function(e){
+	        e.preventDefault();
+	        $(this).parent('div').remove(); //Remove field html
+	        x--; //Decrement field counter
+	    });
+	});
+	// End ---- To add multiple fields\remove jquery function
 
 });
--- resources/views/admin/products/add_attributes.blade.php
+++ resources/views/admin/products/add_attributes.blade.php
@@ -38,13 +38,21 @@
                  <label class="control-label"><strong>{{$productDetails->product_color}}</strong></label>
                 
               </div>    
-
-              <!--div class="control-group">
-                <label class="control-label"> Price</label>
+              <!--  Start ---- To add multiple fields\remove  -->
+              <div class="control-group">   
                 <div class="controls">
-                  <input type="text" name="price" id="price" placeholder="enter atrributes price here">
+                  <div class="field_wrapper">
+                    <div>
+                        <input type="text" name="sku[]" id="sku" placeholder="sku" style="width:120px;" />
+                        <input type="text" name="size[]" id="size" placeholder="size"  style="width:120px;" />
+                        <input type="text" name="price[]" id="price" placeholder="price"  style="width:120px;" />
+                        <input type="text" name="stock[]" id="stock" placeholder="stock"  style="width:120px;" />
+                        <a href="javascript:void(0);" class="add_button icon icon-plus-sign" title="Add field">Add</a>
+                    </div>
+                  </div>                  
                 </div>
-              </div-->
+              </div>
+               <!-- // End ---- To add multiple fields\remove -->
         
             
               <div class="form-actions">
