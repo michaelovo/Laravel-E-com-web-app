64340437851c00552c36e08327f594007d580277
michaelovo <emikeovo@gmail.com>
2019-05-31 03:20:43 -0700

    Modify Admin panel to remove unwanted items

--- resources/views/layouts/admin_layout/admin_sidebar.blade.php
+++ resources/views/layouts/admin_layout/admin_sidebar.blade.php
@@ -1,7 +1,7 @@
 <!--sidebar-menu-->
 <div id="sidebar"><a href="/admin/dashboard" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
   <ul>
-    <li class="active"><a href="index.html"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
+    <li class="active"><a href="/admin/dashboard"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
    
     <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i>
       <span>Categories</span> <span class="label label-important">2</span></a>
@@ -15,12 +15,11 @@
       <span>Products</span> <span class="label label-important">2</span></a>
       <ul>
         <li><a href="{{url('/admin/add-product')}}"><i class="icon icon-plus-sign"></i>Add product</a></li>
-        <li><a href="{{url('/admin/view-product')}}"><i class="icon icon-eye-open"></i>View Products</a></li>
+        <li><a href="{{url('/admin/view-product')}}"><i class="icon icon-eye-open"></i>View Products</a>
         
       </ul>
     </li>
-    <li><a href="buttons.html"><i class="icon icon-tint"></i> <span>Buttons &amp; icons</span></a></li>
-    <li><a href="interface.html"><i class="icon icon-pencil"></i> <span>Eelements</span></a></li>
+    
     <li class="submenu"> <a href="#"><i class="icon icon-cog"></i> <span>Settings</span> <span class="label label-important">1</span></a>
       <ul>
         <li><a href="{{url('/admin/settings')}}"><i class="icon icon-edit"></i>Update password</a></li>
@@ -28,15 +27,7 @@
        
       </ul>
     </li>
-    <li class="submenu"> <a href="#"><i class="icon icon-info-sign"></i> <span>Error</span>
-      <span class="label label-important">4</span></a>
-      <ul>
-        <li><a href="error403.html">Error 403</a></li>
-        <li><a href="error404.html">Error 404</a></li>
-        <li><a href="error405.html">Error 405</a></li>
-        <li><a href="error500.html">Error 500</a></li>
-      </ul>
-    </li>
+    
     <li><a href="{{url('/logout')}}"><i class="icon icon-off"></i> <span>Logout</span></a></li>
   </ul>
 </div>
