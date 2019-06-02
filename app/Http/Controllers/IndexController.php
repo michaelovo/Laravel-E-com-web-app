89792644dc71a4f8093fe1ade17859ab8d6efac1
michaelovo <emikeovo@gmail.com>
2019-06-02 09:59:10 -0700

    Added basic functions to display categories and subcategories on index blade file

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\Category;
 
 class IndexController extends Controller
 {
@@ -11,7 +12,36 @@ class IndexController extends Controller
     	//$productsAll = Product::get(); //ascending order(default)
     	//$productsAll = Product::orderby('id', 'desc')->get(); //descending order
     	$productsAll = Product::inRandomOrder()->get(); //Random order
-        return view('index')->with(compact('productsAll'));
+
+    	
+    	$Categories = Category::where(['parent_id'=>0])->get(); // get all categories and subcategories
+    	//$Categories = json_decode(json_encode($Categories));
+ 		//echo "<pre>"; print_r($Categories); die;
+ 		// $sub_categories = Category::where(['parent_id'=>$cat->id])->get();
+ 		$categories_menu="";
+	    	foreach ($Categories as $cat) {
+	    		$categories_menu.="<div class='panel-heading'>
+									<h4 class='panel-title'>
+										<a data-toggle='collapse' data-parent='#accordian' href='#".$cat->id."'>
+											<span class='badge pull-right'><i class='fa fa-plus'></i></span>
+											".$cat->name."
+										</a>
+									</h4>
+								</div>
+								<div id='".$cat->id."' class='panel-collapse collapse'>
+									<div class='panel-body'>
+										<ul>";
+										$sub_categories = Category::where(['parent_id'=>$cat->id])->get();
+							    		foreach ($sub_categories as $sub_cat) {
+							    			$categories_menu.="<li><a href='".$sub_cat->url."'>".$sub_cat->name." </a></li>";
+							    		}
+											
+											$categories_menu.="</ul>
+									</div>
+								</div>";		
+	    	 	
+	    	}
+        return view('index')->with(compact('productsAll','categories_menu'));
     	
            
     }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,15 +9,15 @@
 						<h2>Category</h2>
 						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
 							<div class="panel panel-default">
-								<div class="panel-heading">
+								<!--div class="panel-heading">
 									<h4 class="panel-title">
 										<a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
 											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
 											Sportswear
 										</a>
 									</h4>
-								</div>
-								<div id="sportswear" class="panel-collapse collapse">
+								</div-->
+								<!--div id="sportswear" class="panel-collapse collapse">
 									<div class="panel-body">
 										<ul>
 											<li><a href="#">Nike </a></li>
@@ -27,7 +27,8 @@
 											<li><a href="#">ASICS </a></li>
 										</ul>
 									</div>
-								</div>
+								</div-->
+								<?php echo $categories_menu; ?>
 							</div>
 							<div class="panel panel-default">
 								<div class="panel-heading">
@@ -56,62 +57,7 @@
 								</div>
 							</div>
 							
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" href="#womens">
-											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-											Womens
-										</a>
-									</h4>
-								</div>
-								<div id="womens" class="panel-collapse collapse">
-									<div class="panel-body">
-										<ul>
-											<li><a href="#">Fendi</a></li>
-											<li><a href="#">Guess</a></li>
-											<li><a href="#">Valentino</a></li>
-											<li><a href="#">Dior</a></li>
-											<li><a href="#">Versace</a></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Kids</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Fashion</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Households</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Interiors</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Clothing</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Bags</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Shoes</a></h4>
-								</div>
-							</div>
+							
 						</div><!--/category-products-->
 					
 						<div class="brands_products"><!--brands_products-->
